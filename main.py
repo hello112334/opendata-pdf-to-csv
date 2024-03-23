8cf0e748e060696dd59d53e9e60c7b6b839366dd
ryo-ma <saka_ro@yahoo.co.jp>
2024-03-23 16:38:55 +0900

    Delete regex=True from newline replace

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ for i, prefecture in enumerate(PREFECTURES, 1):
     # ページごとのデータを結合
     df = pd.concat(dfs)
     # 改行コードを削除
-    df = df.replace('\n', '', regex=True).replace('\r', '', regex=True).replace('\r\n', '', regex=True).replace('\n\r', '', regex=True)
+    df = df.replace('\n', '').replace('\r', '').replace('\r\n', '').replace('\n\r', '')
     # データが2つ未満の行は不要な可能性が高いので行を削除 & 列名に欠損値がある場合も列ごと削除
     result_df = df.dropna(thresh=2).dropna(subset=[df.index[0]], axis=1)
 
